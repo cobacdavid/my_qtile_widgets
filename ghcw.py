225d83d9f74b167d4ba90ffcc01dcdfa6709f7cd
David COBAC <david.cobac@ac-nantes.fr>
2025-12-13 12:16:17 +0100

    gap option changes: search for best value

--- ghcw.py
+++ ghcw.py
@@ -4,7 +4,7 @@
 Description: A Github contribution widget for Qtile
 Author: David COBAC
 Date Created: December 6, 2025
-Date Modified: December 12, 2025
+Date Modified: December 13, 2025
 Version: 1.1
 Python Version: 3.13
 Dependencies: aiohttp, libqtile
@@ -16,6 +16,7 @@ import webbrowser
 from datetime import datetime, timedelta
 
 import aiohttp
+from libqtile.log_utils import logger
 from libqtile.widget import base
 
 QUERY = """
@@ -47,7 +48,7 @@ class Contrib_day:
     def draw(self, ctx):
         ctx.save()
         ctx.translate(self.x, self.y)
-        ctx.rectangle(0, 0, self.dim - 1, self.dim - 1)
+        ctx.rectangle(0, 0, self.dim, self.dim)
         ctx.set_source_rgb(*self.couleur)
         ctx.fill()
         ctx.restore()
@@ -56,20 +57,22 @@ class Contrib_day:
 class Ghcw(base._Widget):
     defaults = [
         ("idgithub", "cobacdavid", ""),
-        ("token", "Your valid token here", ""),
-        ("gap", 1, ""),
+        ("gap", None, ""),
         ("nweeks", 52, ""),
         ("colors",
          [(.1, .1, .1)] + [(0, .2*i, 0) for i in range(1, 6)],
          "")
     ]
 
-    def __init__(self, **config):
+    def __init__(self, token, **config):
         base._Widget.__init__(self, length=0, **config)
         self.add_defaults(self.defaults)
-        # en faisant un fill on perd le pixel du stroke
-        self.gap -= 1
-        # tâche asynchrone pour récupérer les données en API
+        self.token = token
+        #
+        if not self.token:
+            logger.warning("You must provide a valid Github Token.")
+            exit
+        # async task to API
         self._tab_donnees = None
         asyncio.create_task(self.async_init())
         self.add_callbacks({"Button1": self.to_user_webpage})
@@ -82,7 +85,7 @@ class Ghcw(base._Widget):
         """Partie asynchrone de l'init."""
         data = await self.fetch_contribs()
         self._tab_donnees = data
-        # dessine le widget et met à jour la barre
+        # draw widget and update bar
         self.draw()
         self.bar.draw()
 
@@ -109,7 +112,7 @@ class Ghcw(base._Widget):
         for w in weeks:
             for day in w["contributionDays"]:
                 tab_donnees.append((day["date"], day["contributionCount"]))
-        # prepend empty days to align week
+        # append empty days to align week
         missing = 6 - today.weekday()
         for k in range(missing):
             tab_donnees.append(((today + timedelta(days=k+1)).isoformat()[:10],
@@ -119,14 +122,31 @@ class Ghcw(base._Widget):
 
     def draw(self):
         if self._tab_donnees is None:
-            # avant réception des données
+            # no data
             self.drawer.clear(self.background or self.bar.background)
             self.length = 0
             self.draw_at_default_position()
             return
 
+        # no gap is provided, serach for the "best" gap value
+        if self.gap is None:
+            sol = (float('inf'), None)
+            for g in range(1, 10):
+                reste = self.bar.height - 8 * g
+                if reste % 7 == 0 and reste // 7 > 1:
+                    sol = (0, g)
+                    break
+                elif reste < sol[0] and reste // 7 > 1:
+                    sol = (reste, g)
+            self.gap = sol[1] or 0
+            logger.warning("Considering your bar height, "
+                           f"widget ghcw gap's choice is {self.gap}"
+                           "\nYou can force a different value "
+                           "using 'gap' option.")
+
         self.dim = (self.bar.height - 8 * self.gap) // 7
         lgth = self.nweeks * (self.dim + self.gap) + self.gap
+        step = self.dim + self.gap
 
         self.drawer.clear(self.background or self.bar.background)
         ctx = self.drawer.ctx
@@ -135,11 +155,10 @@ class Ghcw(base._Widget):
             for lig in range(7):
                 intensity = self._tab_donnees[col * 7 + lig][1]
                 couleur = self.colors[min(5, int(intensity))]
-                Contrib_day(
-                    self.gap * (col + 1) + col * self.dim,
-                    self.gap * (lig + 1) + lig * self.dim,
-                    self.dim, couleur
-                ).draw(ctx)
+                Contrib_day(col*step,
+                            self.gap + lig*step,
+                            self.dim, couleur).draw(ctx)
 
         self.length = lgth
         self.draw_at_default_position()
+
